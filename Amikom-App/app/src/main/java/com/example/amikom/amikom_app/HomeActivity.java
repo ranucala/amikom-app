063c67233581af85c93f07924f02943ceef6ad87
ranucala <ranucala.p@students.amikom.ac.id>
2017-10-20 10:26:21 +0700

    pertemuan 5

--- Amikom-App/app/src/main/java/com/example/amikom/amikom_app/HomeActivity.java
+++ Amikom-App/app/src/main/java/com/example/amikom/amikom_app/HomeActivity.java
@@ -1,16 +1,22 @@
 package com.example.amikom.amikom_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * Created by amikom on 13/10/2017.
  */
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSimpleList,btnCustomList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +26,23 @@ public class HomeActivity extends AppCompatActivity {
         SharedPreferences sf = getApplicationContext().getSharedPreferences("AmikomPref", Context.MODE_PRIVATE);
         String user = sf.getString("usr",null);
 
+        btnSimpleList = (Button)findViewById(R.id.btnList);
+        btnCustomList = (Button)findViewById(R.id.btnCustom);
+
         usrText.setText("Welcome "+user);
+
+        btnSimpleList.setOnClickListener(this);
+        btnCustomList.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnList:
+                Intent intent = new Intent(this, SimpleList.class);
+                startActivity(intent);
+                break;
+        }
     }
 
 }
--- Amikom-App/app/src/main/java/com/example/amikom/amikom_app/SimpleList.java
+++ Amikom-App/app/src/main/java/com/example/amikom/amikom_app/SimpleList.java
@@ -2,12 +2,30 @@ package com.example.amikom.amikom_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SimpleList extends AppCompatActivity {
 
+    String[] osArray = {
+            "Ubuntu",
+            "Iphone",
+            "Android",
+            "Evercross",
+            "Xiaomi Redmi"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_list);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, osArray
+        );
+        ListView listView = (ListView)findViewById(R.id.list_view);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
